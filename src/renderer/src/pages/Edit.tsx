3f60313ffeb6e9defd9672ad97cd5448df66896f
Vactor0911 <vactor0911@gmail.com>
2024-10-11 20:11:19 +0900

    Added question selector

--- src/renderer/src/pages/Edit.tsx
+++ src/renderer/src/pages/Edit.tsx
@@ -1,49 +1,109 @@
-import { Button, Stack } from "@mui/material"
-import OpenFileButton from "../components/OpenFileButton"
-import { useAtomValue } from "jotai"
-import { csvDataAtom } from "../state"
+import {
+  Button,
+  Divider,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { useAtom, useAtomValue } from "jotai";
+import { csvDataAtom, filePathAtom } from "../state";
+import SaveIcon from "@mui/icons-material/Save";
+import ReplayIcon from "@mui/icons-material/Replay";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import WarningAmberRoundedIcon from "@mui/icons-material/WarningAmberRounded";
+import { isQuestionValid } from "@renderer/util";
 
 const Edit = () => {
-    const [csvData, _] = useAtomValue(csvDataAtom)
+  const [filePath] = useAtomValue(filePathAtom);
+  const [csvData, setCsvData] = useAtom(csvDataAtom);
 
   return (
-    <Stack
-      width={"100vw"}
-      height={"100vh"}
-      direction={"row"}
-      css={{
-        borderTop: "2px solid #e3e3e3"
-      }}
-    >
+    <Stack width={"100vw"} height={"100vh"} direction={"row"} borderTop={"2px solid #e3e3e3"}>
       <Stack
-        width={"30vw"}
-        maxWidth={"400px"}
+        width={"40vw"}
         height={"100%"}
         spacing={2}
-        css={{
-          borderRight: "2px solid #e3e3e3",
-          padding: "10px 30px",
-          justifyContent: csvData.length > 0 ? "spaceBetween" : "center"
-        }}
+        justifyContent={"space-between"}
+        padding={"20px 10px"}
+        borderRight={"2px solid #e3e3e3"}
       >
-        {csvData.length <= 0 && (
-          <>
-            <OpenFileButton />
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => {
-                console.log(csvData)
-              }}
-            >
-              데이터 확인
-            </Button>
-          </>
-        )}
-        {csvData.length > 0 && <></>}
+        <TextField
+          required
+          id="tf-name"
+          label="문제집 제목"
+          variant="outlined"
+          defaultValue={csvData[0][1]}
+        />
+        <Divider />
+
+        <List
+          sx={{
+            overflow: "auto",
+          }}
+        >
+          {csvData.slice(1).map((row, index) => {
+            const flagValid = isQuestionValid(row);
+            return (
+              <ListItemButton
+                key={index}
+                sx={{
+                  overflow: "hidden",
+                }}
+              >
+                <ListItemIcon>
+                  {flagValid ? (
+                    <DescriptionOutlinedIcon color="primary" />
+                  ) : (
+                    <WarningAmberRoundedIcon color="warning" />
+                  )}
+                </ListItemIcon>
+                <ListItemText
+                  primary={row[1]}
+                  primaryTypographyProps={{
+                    fontWeight: "bold",
+                    fontSize: "1.05em",
+                  }}
+                  secondary={row[2]}
+                  css={{
+                    "& span, & p": {
+                      display: "-webkit-box",
+                      WebkitLineClamp: "2",
+                      WebkitBoxOrient: "vertical",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      wordWrap: "break-word",
+                    },
+                    "& p": {
+                      WebkitLineClamp: "1",
+                    },
+                  }}
+                />
+              </ListItemButton>
+            );
+          })}
+        </List>
+
+        <Divider />
+        <Stack spacing={2} minWidth={"200px"} alignSelf={"center"} padding={"0 20px"}>
+          <Button variant="contained" startIcon={<SaveIcon />}>
+            저장하기
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<ReplayIcon />}
+            onClick={() => {
+              setCsvData([[]]);
+            }}
+          >
+            돌아가기
+          </Button>
+        </Stack>
       </Stack>
-      <Stack width={"100%"} height={"100%"} css={{}}></Stack>
+      <Stack width={"auto"} height={"100%"} css={{}}></Stack>
     </Stack>
-  )
-}
+  );
+};
 export default Edit;
--- src/renderer/src/util/index.ts
+++ src/renderer/src/util/index.ts
@@ -10,3 +10,80 @@ export const saveCsvFile = (path, data: string[][]): void => {
   // 파일 저장 경로와 데이터 전달
   window.csvAPI.saveCsvFile(path, csvData);
 };
+
+// 배열 유효성 검사
+const isArrayValid = (arr: string[]): boolean => {
+  let flag = false;
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (arr[i].trim() === "") {
+      if (!flag) {
+        flag = true;
+      }
+    }
+    else if (flag) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/**
+ * 문제 데이터 유효성 검사
+ */
+const questionTypes = ["記述", "選択", "完答", "完答O", "選択完答", "選択完答O", "解説"];
+// 서술형, 선택형, 완성형, 완성형 (순서 상관 있음), 선택 완성형, 선택 완성형 (순서 상관 있음), 해설
+
+export const isQuestionValid = (question: string[]): boolean => {
+  try {
+    if (!questionTypes.includes(question[0])) {
+      return false;
+    }
+
+    switch (question[0]) {
+      case "記述": // 서술형
+        if (question.length < 3) {
+          return false;
+        } else {
+          return isArrayValid(question);
+        }
+        return true;
+      case "選択": // 선택형
+        if (question.length < 4) {
+          return false;
+        } else {
+          return isArrayValid(question);
+        }
+        return true;
+      case "完答":
+      case "完答O": // 완성형
+        if (question.length < 3) {
+          return false;
+        } else {
+          return isArrayValid(question);
+        }
+        return true;
+      case "選択完答":
+      case "選択完答O": // 선택 완성형
+        if (question.length < 4) {
+          return false;
+        } else if (question.length < 4 + Number(question[2]) + Number(question[3])) {
+          return false;
+        } else {
+          return isArrayValid(question);
+        }
+        return true;
+      case "解説": // 해설
+        if (question.length < 2) {
+          return false;
+        } else {
+          return isArrayValid(question);
+        }
+        return true;
+      default:
+        return false;
+    }
+  } catch (e) {
+    return false;
+  }
+};
